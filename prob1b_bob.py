cf35a3cd1b4d3db2c04fa6f423c1a0890b2cf3f8
codingrasshopper <priz.87@gmail.com>
2015-07-12 19:24:02 -0700

    Clean up

--- prob1b_bob.py
+++ prob1b_bob.py
@@ -1,12 +1,8 @@
 # Program to count number of occurences of "Bob"
-import pdb
 s = 'azcbobobobobegghaklbob'
 count = 0
-bobl = 3
 for i in range(len(s)):
     if s[i:i+3] == 'bob':
         count += 1
-    
+ 
 print count
-
-    
